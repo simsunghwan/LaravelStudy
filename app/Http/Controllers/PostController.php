c0dd9436e2b1999af66ca170949f040a0b91b478
simsunghwan <sunghwan1332@g.yju.ac.kr>
2023-10-24 11:34:28 +0900

    2023.10.24수업자료

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -132,7 +132,7 @@ class PostController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        
         // $post = Post::find($id);
 
         // $post->delete();
